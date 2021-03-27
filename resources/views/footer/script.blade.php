6ffc68dfb9dc75ae040ad43eca527ccebdb7dbe6
akindave <akintandavid8@gmail.com>
2021-03-27 20:16:29 +0100

    Adjusted

--- resources/views/footer/script.blade.php
+++ resources/views/footer/script.blade.php
@@ -1,4 +1,5 @@
-    <script src="{{ asset('assets/js/jquery.min.js') }} "></script>
+{{-- this iss  --}}
+<script src="{{ asset('assets/js/jquery.min.js') }} "></script>
 	<script src="{{ asset('assets/js/materialize.min.js') }} "></script>
 	<script src="{{ asset('assets/js/slick.min.js') }} "></script>
 	<script src="{{ asset('assets/js/owl.carousel.min.js') }} "></script>
